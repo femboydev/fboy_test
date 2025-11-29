7fabb7c7141702f6357fcd5032534ccd7fbbccc8
FemboyDev <Unk@poo.com>
2025-11-29 13:33:20 +0000

    test_paycheck

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,25 @@
-import Config from '@common/config';
-import { Greetings } from '@common/index';
-import { addCommand, cache } from '@overextended/ox_lib/server';
+import { GetPlayers } from "@communityox/ox_core/server";
+import config from "../common/config.js";
 
-Greetings();
+setInterval(async () => {
+  const players = await GetPlayers();
 
-if (Config.EnableNuiCommand) {
-  addCommand('openNui', async (playerId) => {
-    if (!playerId) return;
+  console.log(`[fboy] Distributing salary of $${config.SALARY_AMOUNT} to ${players.length} players.`);
 
-    emitNet(`${cache.resource}:openNui`, playerId);
-  });
-}
+  for (const player of players) {
+
+    const account = await player.getAccount();
+
+    if (account) {
+      await account.addBalance(
+        {
+          amount: config.SALARY_AMOUNT,
+          message: "Salary Payment",
+        }
+      );
+      console.log(`[fboy] Added $${config.SALARY_AMOUNT} to player ${player.username} (Account ID: ${account.accountId}).`);
+    }
+  }
+}, 1000 * 60 * config.SALARY_INTERVAL_MINUTES);
+
+console.log("\x1b[35m%s\x1b[0m", "[Femboy Salary] Started salary distribution service .");
